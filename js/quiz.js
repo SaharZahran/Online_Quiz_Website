410f4e801fc18247bb5bcf64cfe012f61530a885
haithamassoli <haitham.b.assoli@gmail.com>
2021-10-25 06:42:26 +0300

    fixed timer

--- js/quiz.js
+++ js/quiz.js
@@ -3,7 +3,6 @@ const question = document.querySelector(".question");
 const allAnswers = document.querySelector(".all-answers");
 const spans = document.querySelector(".spans");
 const container = document.querySelector(".quiz-container");
-const time_line = document.querySelector(".time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 const input = document.querySelectorAll("input");
@@ -50,41 +49,22 @@ submit_Button.addEventListener("click", () => {
     reset();
     loadQuestions(numOfQuestion);
     if (numOfQuestion > 4) {
-      container.innerHTML = "";
-      fetch(
-        "https://raw.githubusercontent.com/SaharZahran/Online_Quiz_Website/main/quiz_questions.json"
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          let counterResult = 0;
-          let quizName = `<h1>${data[quiz_number][0].name}</h1>`;
-          container.insertAdjacentHTML("beforeend", quizName);
-          for (let i = 0; i < data[quiz_number].length; i++) {
-            let questionResult = `</br></br><h3>${data[quiz_number][counterResult]["Question"]}</h3></br>`;
-            container.insertAdjacentHTML("beforeend", questionResult);
-            counterResult++;
-            for (let j = 0; j < data[quiz_number][i].options.length; j++) {
-              let answernResult = `<div class="answer ${trueAns(j)}">
-              <input class="inputRadio" type="radio"><label>${
-                data[quiz_number][i].options[j]
-              }</label></div>`;
-              container.insertAdjacentHTML("beforeend", answernResult);
-            }
-          }
-        });
-    } else {
       calculation();
+      numOfQuestion = 0;
+      correct = 0;
+      loadResult();
     }
     clearInterval(counter);
     startTimer(15);
   }, 700);
 });
 
+console.log(localStorage.getItem("user-answers"));
 function trueAns(index) {
   // console.log(localStorage.getItem("right-answers"));
-  return localStorage.getItem("user-answers").split(",")[index] ==
+  return localStorage.getItem("user-answers").split(",")[index] !=
     localStorage.getItem("right-answers").split(",")[index]
-    ? "correct"
+    ? "incorrect"
     : "";
 }
 // localStorage.clear();
@@ -97,6 +77,63 @@ function createBullets(numOfQuestion) {
       span.classList.add("active-question");
     }
   }
+}
+
+function loadResult() {
+  fetch(
+    "https://raw.githubusercontent.com/SaharZahran/Online_Quiz_Website/main/quiz_questions.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      container.innerHTML = "";
+      let counterResult = 0;
+      // let quizName = `<h1>${data[quiz_number][0].name}</h1>`;
+      // container.insertAdjacentHTML("beforeend", quizName);
+      // for (let i = 0; i < data[quiz_number].length; i++) {
+      //   let questionResult = `</br></br><h3>${data[quiz_number][counterResult]["Question"]}</h3></br>`;
+      //   container.insertAdjacentHTML("beforeend", questionResult);
+      //   counterResult++;
+      //   for (let j = 0; j < data[quiz_number][i].options.length; j++) {
+      //     let answernResult = `<div class="answer ${trueAns(j)}">
+      //     <input class="inputRadio" type="radio"><label>${
+      //       data[quiz_number][i].options[j]
+      //     }</label></div>`;
+      //     container.insertAdjacentHTML("beforeend", answernResult);
+      //   }
+      // }
+
+      let quizName = `<h1>${data[quiz_number][0].name}</h1>`;
+      container.insertAdjacentHTML("beforeend", quizName);
+      for (let i = 0; i < data[quiz_number].length; i++) {
+        let h3 = document.createElement("h3");
+        // data[quiz_number][counterResult]["Question"];
+        h3.innerHTML = data[quiz_number][counterResult]["Question"];
+        container.append(h3);
+        counterResult++;
+        for (let j = 0; j < data[quiz_number][i].options.length; j++) {
+          let div = document.createElement("div");
+          let label = document.createElement("label");
+          div.classList.add("answer");
+          label.innerHTML = data[quiz_number][i].options[j];
+          container.append(div);
+          div.append(label);
+          let userAnswers = localStorage.getItem("user-answers").split(",");
+          // label.classList.add("disabled");
+          // if (
+          //   localStorage.getItem("user-answers").split(",")[j] !=
+          //   localStorage.getItem("right-answers").split(",")[j]
+          // ) {
+          //   console.log("object");
+          //   label.classList.add("incorrect");
+          // }
+          if (
+            data[quiz_number][i].options[j] == data[quiz_number][i].right_answer
+          ) {
+            label.classList.add("correct");
+          }
+        }
+      }
+    });
 }
 
 function addQuestion(arrayOfOptions, number_of_question) {
@@ -121,8 +158,8 @@ function addQuestion(arrayOfOptions, number_of_question) {
   }
 }
 
-let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
-let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+// let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+// let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 function checkRightAnswer(correct_answer) {
   const inputAnswers = document.querySelectorAll("input");
@@ -137,12 +174,12 @@ function checkRightAnswer(correct_answer) {
         input.nextElementSibling.style.color = "#721c24";
         input.nextElementSibling.style.background = "#f8d7da";
         input.nextElementSibling.style.border = "1px solid #f5c6cb";
-        input.insertAdjacentHTML("afterend", crossIconTag);
+        // input.insertAdjacentHTML("afterend", crossIconTag);
       } else {
         input.nextElementSibling.style.color = "#155724";
         input.nextElementSibling.style.background = "#d4edda";
         input.nextElementSibling.style.border = "1px solid #c3e6cb";
-        input.insertAdjacentHTML("afterend", tickIconTag);
+        // input.insertAdjacentHTML("afterend", tickIconTag);
         correct++;
       }
     }
@@ -162,37 +199,45 @@ function storeResult() {
   localStorage.setItem("right-answers", right_answers);
 }
 
+let calculationContainer = 0;
+
 function calculation() {
   number_of_all_user_quizzes++;
-  if (
-    correct >= 3 &&
-    localStorage.getItem("number_of_passed_quizzes") === null
-  ) {
+
+  if (correct >= 3) {
     number_of_passed_quizzes++;
-    localStorage.setItem("number_of_passed_quizzes", number_of_passed_quizzes);
-  } else if (
-    correct >= 3 &&
-    localStorage.getItem("number_of_passed_quizzes") !== null
-  ) {
-    let container = parseInt(localStorage.getItem("number_of_passed_quizzes"));
-    container += 1;
-    localStorage.setItem("number_of_passed_quizzes", container);
   }
-  if (localStorage.getItem("number_of_passed_quizzes") === null) {
-    container = 1;
-  }
-  console.log(container);
-  average_point = (container / number_of_all_user_quizzes) * 100;
-  localStorage.setItem(
-    "number_of_all_user_quizzes",
-    number_of_all_user_quizzes
-  );
-  localStorage.setItem("average_point", average_point);
+  console.log((number_of_passed_quizzes / number_of_all_user_quizzes) * 100);
+  //   if (
+  //     correct >= 3 &&
+  //     localStorage.getItem("number_of_passed_quizzes") === null
+  //   ) {
+  //     number_of_passed_quizzes++;
+  //     localStorage.setItem("number_of_passed_quizzes", number_of_passed_quizzes);
+  //   } else if (
+  //     correct >= 3 &&
+  //     localStorage.getItem("number_of_passed_quizzes") !== null
+  //   ) {
+  //     // calculationContainer = parseInt(
+  //     //   localStorage.getItem("number_of_passed_quizzes")
+  //     // );
+  //     calculationContainer += 1;
+  //     localStorage.setItem("number_of_passed_quizzes", calculationContainer);
+  //   }
+  //   // if (localStorage.getItem("number_of_passed_quizzes") === null) {
+  //   //   calculationContainer = 1;
+  //   // }
+  //   console.log(calculationContainer);
+  //   localStorage.setItem(
+  //     "number_of_all_user_quizzes",
+  //     number_of_all_user_quizzes
+  //   );
+  //   average_point = (+calculationContainer / +number_of_all_user_quizzes) * 100;
+  //   localStorage.setItem("average_point", average_point);
 }
 
 function startTimer(time) {
   counter = setInterval(timer, 1000);
-
   function timer() {
     timeCount.textContent = time;
     time--;
@@ -203,6 +248,11 @@ function startTimer(time) {
     if (time < 0) {
       clearInterval(counter); //clear counter
       timeText.textContent = "Time Off";
+      reset();
+      numOfQuestion++;
+      loadQuestions(numOfQuestion);
+      startTimer(15);
+      numOfQuestion > 4 ? loadResult() : "";
     }
   }
 }
